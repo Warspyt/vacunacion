27c3497d919587f9edc7e89c1a6b513f7ac4a1f2
Tatiana <82002951+Warspyt@users.noreply.github.com>
2021-07-12 01:07:23 -0500

    Update plan_vacunacion.py

--- plan_vacunacion.py
+++ plan_vacunacion.py
@@ -1,50 +1,43 @@
 """ Se importan las librerias para el manejo de las bases de datos
     y de las fechas"""
+import validaciones as vl
 import sqlite3
 from sqlite3 import Error
 from datetime import datetime
 from datetime import date
 
-''' Funcion para establecer la conexion con la base de datos del
-    programa'''
-
 
 class Plan:
-    def __init__(self):
+    def __init__(self, con):
+        self.conexion = con
+        self.cursorObj = con.cursor()
         self.emin = ""
         self.emax = ""
-        self.now = datetime.now()
-        ''' Funcion para crear la tabla de los planes de vacunacion dentro de la base de datos del
-            programa, la cual toma como parametro la conexion de la misma'''
 
-    def tabla_plan(self, con):
+        
+    ''' Funcion para crear la tabla de los planes de vacunacion dentro de la base de datos del
+        programa, la cual toma como parametro la conexion de la misma'''
+
+    def tabla_plan(self):
 
         """ Se crea una tabla para los planes de vacunacion verificando que no exista aun, haciendo uso del objeto cursor
             y el metodo execute que utiliza el CREATE TABLE dentro de los parametros"""
-        cursorObj = con.cursor()
 
-        cursorObj.execute("""CREATE TABLE IF NOT EXISTS PlanVacunacion(idplan integer PRIMARY KEY AUTOINCREMENT, edadmin text,
+        self.cursorObj.execute("""CREATE TABLE IF NOT EXISTS PlanVacunacion(idplan integer PRIMARY KEY AUTOINCREMENT, edadmin text,
                           edadmax text, fechainicioplan text, fechafinalplan text)""")
-        con.commit()
+        self.conexion.commit()
 
-        ''' Funcion para consultar la informacion de los planes de vacunacion activos a la fecha,
-            la cual toma como parametro la conexion con la base de datos del programa'''
 
-    def consultaplan(self, con):
-        cursorObj = con.cursor()
+    ''' Funcion para consultar la informacion de los planes de vacunacion activos a la fecha,
+        la cual toma como parametro la conexion con la base de datos del programa'''
 
-        ''' Se extrae la fecha actual con el metodo now de la libreria datetime y se separa en dia,
-            mes y año'''
-        # now = datetime.now()
-        dia = self.now.strftime("%d")
-        mes = self.now.strftime("%m")
-        ano = self.now.strftime("%Y")
+    def consultaplan(self):
 
         ''' Se extraen todos los planes de vacunacion de la base de datos del programa, con el objeto cursor y el
             metodo execute que utiliza el SELECT como parametro '''
         compara = 'SELECT *FROM PlanVacunacion  '
-        cursorObj.execute(compara)
-        listado = cursorObj.fetchall()
+        self.cursorObj.execute(compara)
+        listado = self.cursorObj.fetchall()
 
         ''' Con el iterador for a partir de una serie de condiciones se filtran los planes de vacunacion activos cuya
             fecha de inicio y fin abarca la fecha actual'''
@@ -56,55 +49,19 @@ class Plan:
             ''' Se extraen las fechas de inicio y fin de cada plan de vacunacion, separandolas en dia, mes
                 y año'''
             ini = ids[3].split("/")
+            fechainicio = vl.Dato(datetime(int(ini[2]), int(ini[1]), int(ini[0])).strftime("%Y/%m/%d"))
             fin = ids[4].split("/")
+            fechafin = vl.Dato(datetime(int(fin[2]), int(fin[1]), int(fin[0])).strftime("%Y/%m/%d"))
 
-            if int(ini[2]) < int(ano):
-                if int(ano) < int(fin[2]):
-                    vigentes.append(ids)
-                elif int(ano) == int(fin[2]):
-                    if int(fin[1]) > int(mes):
-                        vigentes.append(ids)
-                    elif int(fin[1]) == int(mes) and int(fin[2]) >= int(dia):
-                        vigentes.append(ids)
-                    else:
-                        pendientes.append(ids)
-                else:
-                    pendientes.append(ids)
-
-            elif int(ini[2]) == int(ano):
-                if int(ini[1]) < int(mes):
-                    if int(ano) < int(fin[2]):
-                        vigentes.append(ids)
-                    elif int(ano) == int(fin[2]):
-                        if int(fin[1]) > int(mes):
-                            vigentes.append(ids)
-                        elif int(fin[1]) == int(mes) and int(fin[0]) >= int(dia):
-                            vigentes.append(ids)
-                        else:
-                            pendientes.append(ids)
-                    else:
-                        pendientes.append(ids)
-                elif int(ini[1]) == int(mes) and int(ini[0]) <= int(dia):
-                    if int(ano) < int(fin[2]):
-                        vigentes.append(ids)
-                    elif int(ano) == int(fin[2]):
-                        if int(fin[1]) > int(mes):
-                            vigentes.append(ids)
-                        elif int(fin[1]) == int(mes) and int(fin[0]) >= int(dia):
-                            vigentes.append(ids)
-                        else:
-                            pendientes.append(ids)
-                    else:
-                        pendientes.append(ids)
-                else:
-                    pendientes.append(ids)
+            if fechainicio.fecha("<=") and fechafin.fecha(">="):
+                vigentes.append(ids)
             else:
                 pendientes.append(ids)
 
         ''' Se termina la funcion en caso de que la base de datos no tenga informacion o no existan planes de 
             vacunacion activos a la fecha y se notifica al usuario'''
-        if len(vigentes) == 0 and len(pendientes):
-            print("No hay planes de vacunacion en este momento.")
+        if len(vigentes) == 0 and len(pendientes) == 0:
+            print("\nNo hay planes de vacunacion en este momento.\n")
             return
 
         print("\n           PlANES DE VACUNACION ACTIVOS \n")
@@ -140,54 +97,48 @@ class Plan:
         else:
             print("No hay mas planes de vacunacion en este momento.")
 
-        ''' Funcion para guardar la informacion que se le solicita al usuario
-            sobre un plan de vacunacion que se creara'''
+        self.conexion.commit()
 
-    def recibirPlan(self, con):
+    ''' Funcion para guardar la informacion que se le solicita al usuario
+        sobre un plan de vacunacion que se creara'''
+
+    def recibirPlan(self):
 
         """Se solicita al usuario la edad minima y maxima del plan de vacunacion con un bucle que termina cuando
          la informacion es valida, donde se verifica que la edad minima sea menor a la maxima"""
-        emin = 0
-        emax = 0
 
-        while emin >= emax:
+        while True:
 
             ''' Por medio de un bucle se verifica que el dato ingresado para la edad minima sea un valor numerico
                 mayor a cero'''
-            emin = input("Escriba la edad minima del plan: ")
+            emin = vl.Dato(input("Escriba la edad minima del plan: "))
 
-            while True:
-                if emin.isdigit() and len(emin) <= 3 and int(emin) > 0:
-                    break
-                else:
-                    print("Ingrese un valor numerico: ")
-                    emin = input("Escriba la edad minima del plan: ")
+            while not emin.TipoDatoNum() or not emin.longitud(3) or not emin.rango(120):
+                print("Ingrese un valor numerico: ")
+                emin = vl.Dato(input("Escriba la edad minima del plan: "))
 
             ''' Por medio de un bucle se verifica que el dato ingresado para la edad maxima sea un valor numerico
                 mayor a cero'''
-            emax = input("Escriba la edad maxima del plan: ")
+            emax = vl.Dato(input("Escriba la edad maxima del plan: "))
 
-            while True:
-                if emax.isdigit() and len(emax) <= 3 and int(emax) > 0:
-                    break
-                else:
-                    print("Ingrese un valor numerico: ")
-                    emax = input("Escriba la edad maxima del plan: ")
-            emin = int(emin)
-            emax = int(emax)
-            if emin > emax:
-                print("ERROR: la edad minima debe ser mayor a la maxima ingrese datos validos")
-            elif emin <= 0:
-                print("ERROR: la edad minima debe ser mayor a cero ingrese datos validos")
-            elif emax <= 0:
-                print("ERROR: la edad maxima debe ser mayor a cero ingrese datos validos")
+            while not emax.TipoDatoNum() or not emax.longitud(3) or not emax.rango(120):
+                print("Ingrese un valor numerico: ")
+                emax = vl.Dato(input("Escriba la edad maxima del plan: "))
+                
+            if int(emin.variable) > int(emax.variable):
+                print("ERROR: la edad minima debe ser mayor a la maxima ingrese datos validos.")
+            elif int(emin.variable) <= 0:
+                print("ERROR: la edad minima debe ser mayor a cero ingrese datos validos.")
+            elif int(emax.variable) <= 0:
+                print("ERROR: la edad maxima debe ser mayor a cero ingrese datos validos.")
+            else:
+                break
 
-        cursorObj = con.cursor()
-        cursorObj.execute('SELECT * FROM PlanVacunacion')
-        Pexistentes = cursorObj.fetchall()
+        self.cursorObj.execute('SELECT * FROM PlanVacunacion')
+        Pexistentes = self.cursorObj.fetchall()
 
         for ver in Pexistentes:
-            if int(ver[1]) <= emin <= int(ver[2]) or int(ver[1]) <= emax <= int(ver[2]):
+            if int(ver[1]) <= emin.variable <= int(ver[2]) or int(ver[1]) <= emax.variable <= int(ver[2]):
                 print("\nEl rango de edad ingresado o parte de el ya se encuentra dentro del plan de vacunacion numero",
                       ver[0], "que abarca el rango de edad entre los",
                       ver[1], "y los", ver[2], "años.\n")
@@ -195,100 +146,83 @@ class Plan:
 
         ''' Se pide la fecha de inicio del plan por medio de un bucle que se rompe cuando se verifica que la fecha
             ingresada sea mayor a la fecha actual'''
+        
         while True:
 
             ''' Se solicita individualmente el dia, mes y año, verificando a partir de un bucle que los datos sean
                 numericos y existan dentro del calendario'''
-            dini = input("Fecha de inicio del plan:\n\n- Dia de inicio: ")
-            while True:
-                if dini.isdigit() and 0 < int(dini) < 32:
-                    dini = dini.rjust(2, "0")
-                    break
-                else:
-                    dini = input("Escriba el dia de inicio en dos digitos: ")
-            mini = input("- Mes de inicio: ")
-            while True:
-                if mini.isdigit() and 0 < int(mini) < 13:
-                    mini = mini.rjust(2, "0")
-                    break
-                else:
-                    mini = input("Escriba el mes de inicio en numeros entre el 1 y 12: ")
-            aini = input("- año de inicio: ")
-            while True:
-                if aini.isdigit() and len(aini) == 4 and int(aini) >= int(self.now.strftime("%Y")):
-                    aini = aini.rjust(4)
-                    break
-                else:
-                    aini = input("Verifique el año ingresado: ")
+            dini = vl.Dato(input("Fecha de inicio:\n\n- Dia de inicio: "))
+            while not dini.dia():
+                dini = vl.Dato(input("Escriba el dia de inicio en dos digitos: "))
+                
+            mini = vl.Dato(input("- Mes de inicio: "))
+            while not mini.mes():
+                mini = vl.Dato(input("Escriba el mes de inicio en numeros entre el 1 y 12: "))
+                    
+            aini = vl.Dato(input("- año de inicio: "))
+            while not aini.anio(2020, 3000):
+                aini = vl.Dato(input("Escriba el año de inicio en numeros AAAA: "))
 
             ''' Usando el metodo strftime de la libreria datetime se guardan los valores ingresados por el
                 usuario en formato de fecha (DD/MM/AAAA)'''
-            fini1 = datetime(int(aini), int(mini), int(dini)).strftime("%Y/%m/%d")
-            factual = self.now.strftime("%Y/%m/%d")
+            fini1 = vl.Dato(datetime(int(aini.variable), int(mini.variable), int(dini.variable)).strftime("%Y/%m/%d"))
 
-            if fini1 >= factual:
-                fini = datetime(int(aini), int(mini), int(dini)).strftime("%d/%m/%Y")
+            if fini1.fecha(">="):
+                fini = datetime(int(aini.variable), int(mini.variable), int(dini.variable)).strftime("%d/%m/%Y")
                 break
             else:
-                print("La fecha de inicio del plan no es valida: ")
+                print("La fecha de inicio no es valida: ")
         print("Fecha de inicio ingresada: " + fini)
+
 
         ''' Se pide la fecha de fin del plan por medio de un bucle que se rompe cuando se verifica que la fecha
             ingresada sea mayor a la fecha actual y a la fecha de inicio'''
+
         while True:
 
             ''' Se solicita individualmente el dia, mes y año, verificando a partir de un bucle que los datos sean
                 numericos y existan dentro del calendario'''
-            dfin = input("Fecha de finalizacion del plan:\n\n- Dia de finalizacion: ")
-            while True:
-                if dfin.isdigit() and 0 < int(dfin) < 32:
-                    dfin = dfin.rjust(2, "0")
-                    break
-                else:
-                    dfin = input("Escriba el dia de finalizacion en dos digitos: ")
-            mfin = input("- Mes de finalizacion: ")
-            while True:
-                if mfin.isdigit() and 0 < int(mfin) < 13:
-                    mfin = mfin.rjust(2, "0")
-                    break
-                else:
-                    mfin = input("Escriba el mes de finalizacion en numeros entre el 1 y 12: ")
-            afin = input("- año de finalizacion: ")
-            while True:
-                if afin.isdigit() and len(afin) == 4 and int(afin) >= int(self.now.strftime("%Y")):
-                    afin = afin.rjust(4)
-                    break
-                else:
-                    afin = input("Escriba el año de finalizacion en numeros AAAA: ")
+            dfin = vl.Dato(input("Fecha de finalizacion:\n\n- Dia de inicio: "))
+            while not dfin.dia():
+                dfin = vl.Dato(input("Escriba el dia de finalizacion en dos digitos: "))
+                
+            mfin = vl.Dato(input("- Mes de finalizacion: "))
+            while not mfin.mes():
+                mfin = vl.Dato(input("Escriba el mes de finalizacion en numeros entre el 1 y 12: "))
+                    
+            afin = vl.Dato(input("- año de finalizacion: "))
+            while not afin.anio(2020, 3000):
+                afin = vl.Dato(input("Escriba el año de finalizacion en numeros AAAA: "))
 
             ''' Usando el metodo strftime de la libreria datetime se guardan los valores ingresados por el
                 usuario en formato de fecha (DD/MM/AAAA)'''
-            ffin = datetime(int(afin), int(mfin), int(dfin)).strftime("%Y/%m/%d")
-            factual = self.now.strftime("%Y/%m/%d")
+            ffin = vl.Dato(datetime(int(afin.variable), int(mfin.variable), int(dfin.variable)).strftime("%Y/%m/%d"))
 
-            if ffin > factual and ffin > fini1:
-                ffin = datetime(int(afin), int(mfin), int(dfin)).strftime("%d/%m/%Y")
+            if ffin.fecha(">") and ffin.variable > fini1.variable:
+                ffin = datetime(int(afin.variable), int(mfin.variable), int(dfin.variable)).strftime("%d/%m/%Y")
                 break
             else:
-                print("La fecha de finalizacion del plan no es valida: ")
+                print("La fecha de finalizacion no es valida: ")
         print("Fecha de finalizacion ingresada: " + ffin)
+
 
         ''' Se guardan los datos del plan de vacunacion a crear en un contenedor de tipo tupla para su
             posterior uso'''
-        infoplan = (emin, emax, fini, ffin)
+        infoplan = (emin.variable, emax.variable, fini, ffin)
 
         ''' Con el llamado a la funcion asignarvacuna se agenda la cita del paciente sobre el cual se esta iterando'''
-        self.insertar_Plan(con, infoplan)
-        # return datosplan
+        self.insertar_Plan(infoplan)
 
-        ''' Funcion para crear un nuevo lote de vacunas, que toma como parametro la conexion a la
-            base de datos y el contenedor tupla que almacena la informacion del nuevo plan de vacunacion'''
+        self.conexion.commit()
 
-    def insertar_Plan(self, con, datosplan):
+
+    ''' Funcion para crear un nuevo lote de vacunas, que toma como parametro la conexion a la
+        base de datos y el contenedor tupla que almacena la informacion del nuevo plan de vacunacion'''
+
+    def insertar_Plan(self, datosplan):
 
         """ Se crea un nuevo plan de vacunacion con la informacion recolectada del usuario, haciendo uso del
             objeto cursor y el metodo execute que utiliza el INSERT INTO dentro de los parametros"""
-        cursorObj = con.cursor()
-        cursorObj.execute("""INSERT INTO PlanVacunacion( edadmin, edadmax,
+        self.cursorObj.execute("""INSERT INTO PlanVacunacion( edadmin, edadmax,
                           fechainicioplan, fechafinalplan)VALUES ( ?, ?, ?, ?)""", datosplan)
-        con.commit()
+        self.conexion.commit()
